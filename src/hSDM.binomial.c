1092c5b1ed1a59f01ae795d0e5a658e382e400e4
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2016-03-22 09:54:18 +0100

    Reindent lines

--- src/hSDM.binomial.c
+++ src/hSDM.binomial.c
@@ -35,16 +35,16 @@
 /* dens_par */
 
 struct dens_par {
-    /* Data */
-    int NOBS;
-    int *Y;
-    int *T;
-    /* Suitability */
-    int NP;
-    int pos_beta;
-    double **X;
-    double *mubeta, *Vbeta;
-    double *beta_run;  
+  /* Data */
+  int NOBS;
+  int *Y;
+  int *T;
+  /* Suitability */
+  int NP;
+  int pos_beta;
+  double **X;
+  double *mubeta, *Vbeta;
+  double *beta_run;  
 };
 
 
@@ -52,29 +52,29 @@ struct dens_par {
 /* betadens */
 
 static double betadens (double beta_k, void *dens_data) {
-    // Pointer to the structure: d
-    struct dens_par *d;
-    d=dens_data;
-    // Indicating the rank of the parameter of interest
-    int k=d->pos_beta;
-    // logLikelihood
-    double logL=0.0;
-    for (int n=0; n<d->NOBS; n++) {
-	/* theta */
-	double Xpart_theta=0.0;
-	for (int p=0; p<d->NP; p++) {
-	    if (p!=k) {
-		Xpart_theta+=d->X[n][p]*d->beta_run[p];
-	    }
-	}
-	Xpart_theta+=d->X[n][k]*beta_k;
-	double theta=invlogit(Xpart_theta);
-	/* log Likelihood */
-	logL+=dbinom(d->Y[n],d->T[n],theta,1);
+  // Pointer to the structure: d
+  struct dens_par *d;
+  d=dens_data;
+  // Indicating the rank of the parameter of interest
+  int k=d->pos_beta;
+  // logLikelihood
+  double logL=0.0;
+  for (int n=0; n<d->NOBS; n++) {
+    /* theta */
+    double Xpart_theta=0.0;
+    for (int p=0; p<d->NP; p++) {
+      if (p!=k) {
+        Xpart_theta+=d->X[n][p]*d->beta_run[p];
+      }
     }
-    // logPosterior=logL+logPrior
-    double logP=logL+dnorm(beta_k,d->mubeta[k],sqrt(d->Vbeta[k]),1);
-    return logP;
+    Xpart_theta+=d->X[n][k]*beta_k;
+    double theta=invlogit(Xpart_theta);
+    /* log Likelihood */
+    logL+=dbinom(d->Y[n],d->T[n],theta,1);
+  }
+  // logPosterior=logL+logPrior
+  double logP=logL+dnorm(beta_k,d->mubeta[k],sqrt(d->Vbeta[k]),1);
+  return logP;
 }
 
 
@@ -82,7 +82,7 @@ static double betadens (double beta_k, void *dens_data) {
 /* Gibbs sampler function */
 
 void hSDM_binomial (
-	
+    
     // Constants and data
     const int *ngibbs, int *nthin, int *nburn, // Number of iterations, burning and samples
     const int *nobs, // Number of observations
@@ -99,7 +99,7 @@ void hSDM_binomial (
     double *beta_vect,
     // Defining priors
     const double *mubeta, double *Vbeta,
-     // Diagnostic
+    // Diagnostic
     double *Deviance,
     double *theta_latent, // Latent proba of suitability (length NOBS) 
     double *theta_pred, // Proba of suitability for predictions (length NPRED)
@@ -109,261 +109,261 @@ void hSDM_binomial (
     const int *verbose,
     // Save p
     const int *save_p
-    
-    ) {
-	
-    ////////////////////////////////////////////////////////////////////////////////
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    // Defining and initializing objects
-
-    ////////////////////////////////////////
-    // Initialize random number generator //
-    srand(seed[0]);
-    
-    ///////////////////////////
-    // Redefining constants //
-    const int NGIBBS=ngibbs[0];
-    const int NTHIN=nthin[0];
-    const int NBURN=nburn[0];
-    const int NSAMP=(NGIBBS-NBURN)/NTHIN;
-    const int NOBS=nobs[0];
-    const int NP=np[0];
-    const int NPRED=npred[0];
-
-    ///////////////////////////////////
-    // Declaring some useful objects //
-    double *theta_run=malloc(NOBS*sizeof(double));
-    for (int n=0; n<NOBS; n++) {
-	theta_run[n]=0.0;
-    }
-    double *theta_pred_run=malloc(NPRED*sizeof(double));
-    for (int m=0; m<NPRED; m++) {
-	theta_pred_run[m]=0.0;
-    }
-
-    //////////////////////////////////////////////////////////
-    // Set up and initialize structure for density function //
-    struct dens_par dens_data;
-
-    /* Data */
-    dens_data.NOBS=NOBS;
-    // Y
-    dens_data.Y=malloc(NOBS*sizeof(int));
-    for (int n=0; n<NOBS; n++) {
-	dens_data.Y[n]=Y_vect[n];
-    }
-    // T
-    dens_data.T=malloc(NOBS*sizeof(int));
-    for (int n=0; n<NOBS; n++) {
-	dens_data.T[n]=T_vect[n];
-    }
-
-    /* Suitability process */
-    dens_data.NP=NP;
-    dens_data.pos_beta=0;
-    dens_data.X=malloc(NOBS*sizeof(double*));
-    for (int n=0; n<NOBS; n++) {
-    	dens_data.X[n]=malloc(NP*sizeof(double));
-    	for (int p=0; p<NP; p++) {
-    	    dens_data.X[n][p]=X_vect[p*NOBS+n];
-    	}
-    }
-    dens_data.mubeta=malloc(NP*sizeof(double));
-    dens_data.Vbeta=malloc(NP*sizeof(double));
+  
+) {
+  
+  ////////////////////////////////////////////////////////////////////////////////
+  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // Defining and initializing objects
+  
+  ////////////////////////////////////////
+  // Initialize random number generator //
+  srand(seed[0]);
+  
+  ///////////////////////////
+  // Redefining constants //
+  const int NGIBBS=ngibbs[0];
+  const int NTHIN=nthin[0];
+  const int NBURN=nburn[0];
+  const int NSAMP=(NGIBBS-NBURN)/NTHIN;
+  const int NOBS=nobs[0];
+  const int NP=np[0];
+  const int NPRED=npred[0];
+  
+  ///////////////////////////////////
+  // Declaring some useful objects //
+  double *theta_run=malloc(NOBS*sizeof(double));
+  for (int n=0; n<NOBS; n++) {
+    theta_run[n]=0.0;
+  }
+  double *theta_pred_run=malloc(NPRED*sizeof(double));
+  for (int m=0; m<NPRED; m++) {
+    theta_pred_run[m]=0.0;
+  }
+  
+  //////////////////////////////////////////////////////////
+  // Set up and initialize structure for density function //
+  struct dens_par dens_data;
+  
+  /* Data */
+  dens_data.NOBS=NOBS;
+  // Y
+  dens_data.Y=malloc(NOBS*sizeof(int));
+  for (int n=0; n<NOBS; n++) {
+    dens_data.Y[n]=Y_vect[n];
+  }
+  // T
+  dens_data.T=malloc(NOBS*sizeof(int));
+  for (int n=0; n<NOBS; n++) {
+    dens_data.T[n]=T_vect[n];
+  }
+  
+  /* Suitability process */
+  dens_data.NP=NP;
+  dens_data.pos_beta=0;
+  dens_data.X=malloc(NOBS*sizeof(double*));
+  for (int n=0; n<NOBS; n++) {
+    dens_data.X[n]=malloc(NP*sizeof(double));
     for (int p=0; p<NP; p++) {
-	dens_data.mubeta[p]=mubeta[p];
-	dens_data.Vbeta[p]=Vbeta[p];
+      dens_data.X[n][p]=X_vect[p*NOBS+n];
     }
-    dens_data.beta_run=malloc(NP*sizeof(double));
+  }
+  dens_data.mubeta=malloc(NP*sizeof(double));
+  dens_data.Vbeta=malloc(NP*sizeof(double));
+  for (int p=0; p<NP; p++) {
+    dens_data.mubeta[p]=mubeta[p];
+    dens_data.Vbeta[p]=Vbeta[p];
+  }
+  dens_data.beta_run=malloc(NP*sizeof(double));
+  for (int p=0; p<NP; p++) {
+    dens_data.beta_run[p]=beta_start[p];
+  }
+  
+  /* Predictions */
+  // X_pred
+  double **X_pred=malloc(NPRED*sizeof(double*));
+  for (int m=0; m<NPRED; m++) {
+    X_pred[m]=malloc(NP*sizeof(double));
     for (int p=0; p<NP; p++) {
-	dens_data.beta_run[p]=beta_start[p];
+      X_pred[m][p]=X_pred_vect[p*NPRED+m];
     }
-
-    /* Predictions */
-    // X_pred
-    double **X_pred=malloc(NPRED*sizeof(double*));
-    for (int m=0; m<NPRED; m++) {
-    	X_pred[m]=malloc(NP*sizeof(double));
-    	for (int p=0; p<NP; p++) {
-    	    X_pred[m][p]=X_pred_vect[p*NPRED+m];
-    	}
-    }
-
-    ////////////////////////////////////////////////////////////
-    // Proposal variance and acceptance for adaptive sampling //
-
+  }
+  
+  ////////////////////////////////////////////////////////////
+  // Proposal variance and acceptance for adaptive sampling //
+  
+  // beta
+  double *sigmap_beta = malloc(NP*sizeof(double));
+  int *nA_beta = malloc(NP*sizeof(int));
+  double *Ar_beta = malloc(NP*sizeof(double)); // Acceptance rate 
+  for (int p=0; p<NP; p++) {
+    nA_beta[p]=0;
+    sigmap_beta[p]=1.0;
+    Ar_beta[p]=0.0;
+  }
+  
+  ////////////
+  // Message//
+  Rprintf("\nRunning the Gibbs sampler. It may be long, please keep cool :)\n\n");
+  R_FlushConsole();
+  //R_ProcessEvents(); for windows
+  
+  ///////////////////////////////////////////////////////////////////////////////////////
+  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // Gibbs sampler
+  
+  for (int g=0;g<NGIBBS;g++) {
+    
+    ////////////////////////////////////////////////
     // beta
-    double *sigmap_beta = malloc(NP*sizeof(double));
-    int *nA_beta = malloc(NP*sizeof(int));
-    double *Ar_beta = malloc(NP*sizeof(double)); // Acceptance rate 
-    for (int p=0; p<NP; p++) {
-	nA_beta[p]=0;
-	sigmap_beta[p]=1.0;
-	Ar_beta[p]=0.0;
-    }
-
-    ////////////
-    // Message//
-    Rprintf("\nRunning the Gibbs sampler. It may be long, please keep cool :)\n\n");
-    R_FlushConsole();
-    //R_ProcessEvents(); for windows
-
-    ///////////////////////////////////////////////////////////////////////////////////////
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    // Gibbs sampler
-
-    for (int g=0;g<NGIBBS;g++) {
-
-	////////////////////////////////////////////////
-	// beta
-	
-	for (int p=0; p<NP; p++) {
-	    dens_data.pos_beta=p; // Specifying the rank of the parameter of interest
-	    double x_now=dens_data.beta_run[p];
-	    double x_prop=myrnorm(x_now,sigmap_beta[p]);
-	    double p_now=betadens(x_now, &dens_data);
-	    double p_prop=betadens(x_prop, &dens_data);
-	    double r=exp(p_prop-p_now); // ratio
-	    double z=myrunif();	
-	    // Actualization
-	    if (z < r) {
-		dens_data.beta_run[p]=x_prop;
-		nA_beta[p]++;
-	    }
-	}
-
-
-	//////////////////////////////////////////////////
-	// Deviance
-
-	// logLikelihood
-	double logL=0.0;
-	for (int n=0; n<NOBS; n++) {
-	    /* theta */
-	    double Xpart_theta=0.0;
-	    for (int p=0; p<NP; p++) {
-		Xpart_theta+=dens_data.X[n][p]*dens_data.beta_run[p];
-	    }
-	    theta_run[n]=invlogit(Xpart_theta);
-	    /* log Likelihood */
-	    logL+=dbinom(dens_data.Y[n],dens_data.T[n],theta_run[n],1);
-	}
-
-	// Deviance
-	double Deviance_run=-2*logL;
-
-
-	//////////////////////////////////////////////////
-	// Predictions
-	for (int m=0; m<NPRED; m++) {
-	    /* theta_pred_run */
-	    double Xpart_theta_pred=0.0;
-	    for (int p=0; p<NP; p++) {
-		Xpart_theta_pred+=X_pred[m][p]*dens_data.beta_run[p];
-	    }
-	    theta_pred_run[m]=invlogit(Xpart_theta_pred);
-	}
-
-
-	//////////////////////////////////////////////////
-	// Output
-	if (((g+1)>NBURN) && (((g+1)%(NTHIN))==0)) {
-	    int isamp=((g+1)-NBURN)/(NTHIN);
-	    for (int p=0; p<NP; p++) {
-		beta_vect[p*NSAMP+(isamp-1)]=dens_data.beta_run[p];
-	    }
-	    Deviance[isamp-1]=Deviance_run;
-	    for (int n=0; n<NOBS; n++) {
-		theta_latent[n]+=theta_run[n]/NSAMP; // We compute the mean of NSAMP values
-	    }
-	    // prob.p
-	    if (save_p[0]==0) { // We compute the mean of NSAMP values
-		for (int m=0; m<NPRED; m++) {
-		    theta_pred[m]+=theta_pred_run[m]/NSAMP; 
-		}
-	    }
-	    if (save_p[0]==1) { // The NSAMP sampled values for theta are saved
-		for (int m=0; m<NPRED; m++) {
-		    theta_pred[m*NSAMP+(isamp-1)]=theta_pred_run[m]; 
-		}
-	    }
-	}
-
     
-	///////////////////////////////////////////////////////
-	// Adaptive sampling (on the burnin period)
-	const double ropt=0.44;
-	int DIV=0;
-	if (NGIBBS >=1000) DIV=100;
-	else DIV=NGIBBS/10;
-	/* During the burnin period */
-	if((g+1)%DIV==0 && (g+1)<=NBURN){
-	    for (int p=0; p<NP; p++) {
-		Ar_beta[p]=((double) nA_beta[p])/DIV;
-		if(Ar_beta[p]>=ropt) sigmap_beta[p]=sigmap_beta[p]*(2-(1-Ar_beta[p])/(1-ropt));
-		else sigmap_beta[p]=sigmap_beta[p]/(2-Ar_beta[p]/ropt);
-		nA_beta[p]=0.0; // We reinitialize the number of acceptance to zero
-	    }
-	}
-        /* After the burnin period */
-	if((g+1)%DIV==0 && (g+1)>NBURN){
-	    for (int p=0; p<NP; p++) {
-		Ar_beta[p]=((double) nA_beta[p])/DIV;
-		nA_beta[p]=0.0; // We reinitialize the number of acceptance to zero
-	    }
-	}
-
-
-	//////////////////////////////////////////////////
-	// Progress bar
-	double Perc=100*(g+1)/(NGIBBS);
-	if (((g+1)%(NGIBBS/100))==0 && verbose[0]==1) {  
-	    Rprintf("*");
-	    R_FlushConsole();
-	    //R_ProcessEvents(); for windows
-	    if(((g+1)%(NGIBBS/10))==0){
-		double mAr_beta=0; // Mean acceptance rate
-		for (int p=0; p<NP; p++) {
-		    mAr_beta+=Ar_beta[p]/NP;
-		}
-		Rprintf(":%.1f%%, mean accept. rates= beta:%.3f\n",Perc,mAr_beta);
-		R_FlushConsole();
-		//R_ProcessEvents(); for windows
-	    }
-	} 
-
-
-	//////////////////////////////////////////////////
-	// User interrupt
-	R_CheckUserInterrupt(); // allow user interrupt 	    
-	
-    } // Gibbs sampler
-
-    ///////////////
-    // Delete memory allocation (see malloc())
-    /* Data */
-    free(dens_data.Y);
-    free(dens_data.T);
-    /* Suitability */
+    for (int p=0; p<NP; p++) {
+      dens_data.pos_beta=p; // Specifying the rank of the parameter of interest
+      double x_now=dens_data.beta_run[p];
+      double x_prop=myrnorm(x_now,sigmap_beta[p]);
+      double p_now=betadens(x_now, &dens_data);
+      double p_prop=betadens(x_prop, &dens_data);
+      double r=exp(p_prop-p_now); // ratio
+      double z=myrunif();	
+      // Actualization
+      if (z < r) {
+        dens_data.beta_run[p]=x_prop;
+        nA_beta[p]++;
+      }
+    }
+    
+    
+    //////////////////////////////////////////////////
+    // Deviance
+    
+    // logLikelihood
+    double logL=0.0;
     for (int n=0; n<NOBS; n++) {
-    	free(dens_data.X[n]);
+      /* theta */
+      double Xpart_theta=0.0;
+      for (int p=0; p<NP; p++) {
+        Xpart_theta+=dens_data.X[n][p]*dens_data.beta_run[p];
+      }
+      theta_run[n]=invlogit(Xpart_theta);
+      /* log Likelihood */
+      logL+=dbinom(dens_data.Y[n],dens_data.T[n],theta_run[n],1);
     }
-    free(dens_data.X);
-    free(dens_data.mubeta);
-    free(dens_data.Vbeta);
-    free(dens_data.beta_run);
-    free(theta_run);
-    /* Predictions */
+    
+    // Deviance
+    double Deviance_run=-2*logL;
+    
+    
+    //////////////////////////////////////////////////
+    // Predictions
     for (int m=0; m<NPRED; m++) {
-    	free(X_pred[m]);
+      /* theta_pred_run */
+      double Xpart_theta_pred=0.0;
+      for (int p=0; p<NP; p++) {
+        Xpart_theta_pred+=X_pred[m][p]*dens_data.beta_run[p];
+      }
+      theta_pred_run[m]=invlogit(Xpart_theta_pred);
     }
-    free(X_pred);
-    free(theta_pred_run);
-    /* Adaptive MH */
-    free(sigmap_beta);
-    free(nA_beta);
-    free(Ar_beta);
-
+    
+    
+    //////////////////////////////////////////////////
+    // Output
+    if (((g+1)>NBURN) && (((g+1)%(NTHIN))==0)) {
+      int isamp=((g+1)-NBURN)/(NTHIN);
+      for (int p=0; p<NP; p++) {
+        beta_vect[p*NSAMP+(isamp-1)]=dens_data.beta_run[p];
+      }
+      Deviance[isamp-1]=Deviance_run;
+      for (int n=0; n<NOBS; n++) {
+        theta_latent[n]+=theta_run[n]/NSAMP; // We compute the mean of NSAMP values
+      }
+      // prob.p
+      if (save_p[0]==0) { // We compute the mean of NSAMP values
+        for (int m=0; m<NPRED; m++) {
+          theta_pred[m]+=theta_pred_run[m]/NSAMP; 
+        }
+      }
+      if (save_p[0]==1) { // The NSAMP sampled values for theta are saved
+        for (int m=0; m<NPRED; m++) {
+          theta_pred[m*NSAMP+(isamp-1)]=theta_pred_run[m]; 
+        }
+      }
+    }
+    
+    
+    ///////////////////////////////////////////////////////
+    // Adaptive sampling (on the burnin period)
+    const double ropt=0.44;
+    int DIV=0;
+    if (NGIBBS >=1000) DIV=100;
+    else DIV=NGIBBS/10;
+    /* During the burnin period */
+    if((g+1)%DIV==0 && (g+1)<=NBURN){
+      for (int p=0; p<NP; p++) {
+        Ar_beta[p]=((double) nA_beta[p])/DIV;
+        if(Ar_beta[p]>=ropt) sigmap_beta[p]=sigmap_beta[p]*(2-(1-Ar_beta[p])/(1-ropt));
+        else sigmap_beta[p]=sigmap_beta[p]/(2-Ar_beta[p]/ropt);
+        nA_beta[p]=0.0; // We reinitialize the number of acceptance to zero
+      }
+    }
+    /* After the burnin period */
+    if((g+1)%DIV==0 && (g+1)>NBURN){
+      for (int p=0; p<NP; p++) {
+        Ar_beta[p]=((double) nA_beta[p])/DIV;
+        nA_beta[p]=0.0; // We reinitialize the number of acceptance to zero
+      }
+    }
+    
+    
+    //////////////////////////////////////////////////
+    // Progress bar
+    double Perc=100*(g+1)/(NGIBBS);
+    if (((g+1)%(NGIBBS/100))==0 && verbose[0]==1) {  
+      Rprintf("*");
+      R_FlushConsole();
+      //R_ProcessEvents(); for windows
+      if(((g+1)%(NGIBBS/10))==0){
+        double mAr_beta=0; // Mean acceptance rate
+        for (int p=0; p<NP; p++) {
+          mAr_beta+=Ar_beta[p]/NP;
+        }
+        Rprintf(":%.1f%%, mean accept. rates= beta:%.3f\n",Perc,mAr_beta);
+        R_FlushConsole();
+        //R_ProcessEvents(); for windows
+      }
+    } 
+    
+    
+    //////////////////////////////////////////////////
+    // User interrupt
+    R_CheckUserInterrupt(); // allow user interrupt 	    
+    
+  } // Gibbs sampler
+  
+  ///////////////
+  // Delete memory allocation (see malloc())
+  /* Data */
+  free(dens_data.Y);
+  free(dens_data.T);
+  /* Suitability */
+  for (int n=0; n<NOBS; n++) {
+    free(dens_data.X[n]);
+  }
+  free(dens_data.X);
+  free(dens_data.mubeta);
+  free(dens_data.Vbeta);
+  free(dens_data.beta_run);
+  free(theta_run);
+  /* Predictions */
+  for (int m=0; m<NPRED; m++) {
+    free(X_pred[m]);
+  }
+  free(X_pred);
+  free(theta_pred_run);
+  /* Adaptive MH */
+  free(sigmap_beta);
+  free(nA_beta);
+  free(Ar_beta);
+  
 } // end hSDM function
 
 
--- src/useful.c
+++ src/useful.c
@@ -6,18 +6,18 @@
 /* General functions */
 /*****************************************************************/
 
-/**********************
+/**********************/
 /* Function logit */
 double logit (double x) {
-    double Result=log(x)-log(1-x);
-    return Result;
+  double Result=log(x)-log(1-x);
+  return Result;
 }
 
-/**********************
+/**********************/
 /* Function invlogit */
 double invlogit (double x) {
-    double Result=1/(1+exp(-x));
-    return Result;
+  double Result=1/(1+exp(-x));
+  return Result;
 }
 
 
@@ -25,61 +25,61 @@ double invlogit (double x) {
 /* Random draws */
 /*****************************************************************/
 
-/**********************
+/**********************/
 /* myrunif() */
 double myrunif() {
-    return ((double)rand() + 0.5)/((double)RAND_MAX + 1.0);
+  return ((double)rand() + 0.5)/((double)RAND_MAX + 1.0);
 }
 
-/**********************
+/**********************/
 /* myrgamma1(shape,rate=1), function modified from the Scythe C++ library */
 double myrgamma1 (double alpha) {
-
-    double accept;
-    int test;
-    double u, v, w, x, y, z, b, c;
+  
+  double accept;
+  int test;
+  double u, v, w, x, y, z, b, c;
+  
+  // Implement Best's (1978) simulator
+  b = alpha - 1;
+  c = 3 * alpha - 0.75;
+  test = 0;
+  while (test == 0) {
+    u = myrunif();
+    v = myrunif();
     
-    // Implement Best's (1978) simulator
-    b = alpha - 1;
-    c = 3 * alpha - 0.75;
-    test = 0;
-    while (test == 0) {
-	u = myrunif();
-	v = myrunif();
-	
-	w = u * (1 - u);
-	y = sqrt (c / w) * (u - .5);
-	x = b + y;
-
-	if (x > 0) {
-            z = 64 * pow (v, 2) * pow (w, 3);
-            if (z <= (1 - (2 * pow (y, 2) / x))) {
-		test = 1;
-		accept = x;
-            } else if ((2 * (b * log (x / b) - y)) >= log (z)) {
-		test = 1;
-		accept = x;
-            } else {
-		test = 0;
-            }
-	}
+    w = u * (1 - u);
+    y = sqrt (c / w) * (u - .5);
+    x = b + y;
+    
+    if (x > 0) {
+      z = 64 * pow (v, 2) * pow (w, 3);
+      if (z <= (1 - (2 * pow (y, 2) / x))) {
+        test = 1;
+        accept = x;
+      } else if ((2 * (b * log (x / b) - y)) >= log (z)) {
+        test = 1;
+        accept = x;
+      } else {
+        test = 0;
+      }
     }
-    
-    return accept;
+  }
+  
+  return accept;
 }
 
-/**********************
+/**********************/
 /* rnorm1, Knuth's 2nd volume of TAOCP 3rd edition page 122 */
 double rnorm1 () {
   double v1,v2,s;
-
+  
   do {
     v1 = 2.0 * ((double) rand()/RAND_MAX) - 1;
     v2 = 2.0 * ((double) rand()/RAND_MAX) - 1;
-
+    
     s = v1*v1 + v2*v2;
   } while ( s >= 1.0 );
-
+  
   if (s == 0.0)
     return 0.0;
   else
@@ -88,12 +88,12 @@ double rnorm1 () {
 
 /* myrnorm */
 double myrnorm (double mean, double sd) {
-    return (mean + rnorm1() * sd);
+  return (mean + rnorm1() * sd);
 }
 
-/**********************
-/* left truncated gamma
-/* Philippe, A. Simulation of right and left truncated gamma distributions by mixtures Statistics and Computing, 1997, 7, 173-181
+/************************/
+/* left truncated gamma */
+/* Philippe, A. Simulation of right and left truncated gamma distributions by mixtures Statistics and Computing, 1997, 7, 173-181 */
 
 /* the following function returns a random number from TG^+(a,b,1)            */
 /* a=shape parameter, b=rate parameter                                        */
@@ -102,75 +102,75 @@ double myrnorm (double mean, double sd) {
 /* Springer-Verlag, New-York.                                                 */
 double integer(double a, double b)                             
 {
-        double u,x;
-        double *wl,*wlc;
-        int i;
-        wl=(double *)malloc((int)(a+1)*sizeof(double));
-        wlc=(double *)malloc((int)(a+1)*sizeof(double));
-        wl[1]=1.0;
-        wlc[1]=1.0;
-        for(i=2; i<= (int)a ;i++ )
-                {
-                 wl[i]=wl[i-1]*(a-i+1)/b;
-                 wlc[i]=wlc[i-1]+wl[i];
-                 };
-        for(i=1; i<= (int) a; i++)
-                 {
-                wlc[i]=wlc[i]/wlc[(int)a];
-                 };
-        u=myrunif();
-        i=1;
-        while(u>wlc[i]){i=i+1;};
-        x=myrgamma1( (double) i)/b+1.0;        
-        free(wl);
-        free(wlc);               
-        return(x);
+  double u,x;
+  double *wl,*wlc;
+  int i;
+  wl=(double *)malloc((int)(a+1)*sizeof(double));
+  wlc=(double *)malloc((int)(a+1)*sizeof(double));
+  wl[1]=1.0;
+  wlc[1]=1.0;
+  for(i=2; i<= (int)a ;i++ )
+  {
+    wl[i]=wl[i-1]*(a-i+1)/b;
+    wlc[i]=wlc[i-1]+wl[i];
+  };
+  for(i=1; i<= (int) a; i++)
+  {
+    wlc[i]=wlc[i]/wlc[(int)a];
+  };
+  u=myrunif();
+  i=1;
+  while(u>wlc[i]){i=i+1;};
+  x=myrgamma1( (double) i)/b+1.0;        
+  free(wl);
+  free(wlc);               
+  return(x);
 }
 
 /* the following function returns a random number from TG^+(a,b,1)            */  
 /* a=shape parameter, b=rate parameter                                        */ 
 double inter_le(double a, double b)
 {
-        double test=0,u,x,y,M;
-        if (a<1.0)
-            {
-            M=1.0;
-            while (test == 0)
-                         {
-                         x=1-(1/b)*log(1-myrunif());
-                         y=1/pow(x,1-a);
-                         if (myrunif()< y/M) test=1.0;
-                         };
-            }
-            else
-            {
-            if (a<b) 
-                      {
-                       M=exp(floor(a)-a);
-                       while (test == 0)
-                            {
-                            x=integer(floor(a), b*floor(a)/a);
-                            y=pow(x, a-floor(a))*exp(-x*b*(1-floor(a)/a));
-                            if (myrunif()< y/M) test=1.0;
-                            };
-                       }
-                       else
-                       {
-                        M=exp(floor(a)-a)*pow(a/b,a-floor(a));
-                        while (test == 0)
-                           {
-                            x=integer(floor(a), b+floor(a)-a);
-                            y=pow(x, a-floor(a))*exp(-x*(-floor(a)+a));
-                            if (myrunif()< y/M) test=1.0;
-                            };
-		       };
-	   };
-         return(x);
+  double test=0,u,x,y,M;
+  if (a<1.0)
+  {
+    M=1.0;
+    while (test == 0)
+    {
+      x=1-(1/b)*log(1-myrunif());
+      y=1/pow(x,1-a);
+      if (myrunif()< y/M) test=1.0;
+    };
+  }
+  else
+  {
+    if (a<b) 
+    {
+      M=exp(floor(a)-a);
+      while (test == 0)
+      {
+        x=integer(floor(a), b*floor(a)/a);
+        y=pow(x, a-floor(a))*exp(-x*b*(1-floor(a)/a));
+        if (myrunif()< y/M) test=1.0;
+      };
+    }
+    else
+    {
+      M=exp(floor(a)-a)*pow(a/b,a-floor(a));
+      while (test == 0)
+      {
+        x=integer(floor(a), b+floor(a)-a);
+        y=pow(x, a-floor(a))*exp(-x*(-floor(a)+a));
+        if (myrunif()< y/M) test=1.0;
+      };
+    };
+  };
+  return(x);
 }
 
 /* the following function returns a random number from TG^+(a,b,t)            */
 /* a=shape parameter, b=rate parameter                                        */
 double myrtgamma_left(double a, double b, double t)                                 
 {
-        return(inter_le(a, b*t)*t);
+  return(inter_le(a, b*t)*t);
 }
