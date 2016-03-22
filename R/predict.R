5b9a34a13ee16b31e7c4a04174e36ec8cb7f0d4a
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2016-03-22 11:53:33 +0100

    predict() in place of predict.hSDM()

--- R/predict.R
+++ R/predict.R
@@ -1,6 +1,6 @@
 ####################################################################
 ##
-## predict.hSDM.R
+## predict.R
 ##
 ####################################################################
 ##
@@ -18,7 +18,7 @@
 ## 
 ####################################################################
 
-predict.hSDM <- function(object,newdata=NULL,type="mean",probs=c(0.025,0.975)) {
+predict <- function(object,newdata=NULL,type="mean",probs=c(0.025,0.975),...) {
 
     ##= Check
     if (!(type %in% c("mean","quantile","posterior"))) {stop("type must be \"mean\", \"quantile\" or \"posterior\"")}
